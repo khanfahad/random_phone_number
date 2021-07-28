683c7236ac20813200b461213403fcc524b99205
Fahad Khan <khanfahad@gmail.com>
2021-07-28 13:48:21 -0500

    Create phone_number_generator.py
    Generates a random 11-digit phone number with the country code "+1"

---
+++ phone_number_generator.py
@@ -0,0 +1,11 @@
+import random
+import time
+
+def main(): 
+
+  while True: 
+    print('+1-' + str(random.randint(100,999)) + '-' + str(random.randint(100,999)) + '-' + str(random.randint(1000,9999)))
+    time.sleep(1)
+
+    
+main() 
